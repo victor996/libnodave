f9e6c62a3167749ff6b27a915064a706ff1f3118
Snail Mail <>
2017-06-14 13:38:47 +0000

    updates to compile usb2mpi driver for kernel 4.9

--- usb2mpi-Linux-2.6/siemens_mpi.c
+++ usb2mpi-Linux-2.6/siemens_mpi.c
@@ -1,28 +1,30 @@
 /*
  * Siemens USB-MPI Serial USB driver
- * 
+ *
  * Copyright (C) 2005 Thomas Hergenhahn <thomas.hergenhahn@suse.de>
- * 
- * Largely based on siemens_usb_mpi.c, 
+ *
+ * Largely based on siemens_usb_mpi.c,
  * Copyright (C) 2005 Greg Kroah-Hartman <gregkh@suse.de>
  *
  *	This program is free software; you can redistribute it and/or
  *	modify it under the terms of the GNU General Public License version
  *	2 as published by the Free Software Foundation.
  */
-
+//#include <generated/autoconf.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/tty.h>
 #include <linux/module.h>
 #include <linux/usb.h>
-#include "usb-serial.h"
+//#include "usb-serial.h"
+#include <linux/usb/serial.h>
+#include <linux/serial.h>
 
 /* Version Information */
 #define DRIVER_VERSION "Version 0.1 09/26/2005"
 #define DRIVER_AUTHOR "Thomas Hergenhahn@web.de http://libnodave.sf.net"
 #define DRIVER_DESC "Driver for Siemens USB/MPI adapter"
-
+#define NAME "Siemens USB-MPI"
 
 static struct usb_device_id id_table [] = {
 	{ USB_DEVICE(0x908, 0x0004) },	// Vendor and product id for 6ES7-972-0CB20-0XA0
@@ -30,12 +32,13 @@ static struct usb_device_id id_table [] = {
 };
 MODULE_DEVICE_TABLE(usb, id_table);
 
+
 static struct usb_driver siemens_usb_mpi_driver = {
-	.owner =	THIS_MODULE,
-	.name =		"Siemens USB-MPI",
-	.probe =	usb_serial_probe,
-	.disconnect =	usb_serial_disconnect,
+	// .owner =	THIS_MODULE,
+	.name = NAME,
 	.id_table =	id_table,
+	// .probe =	usb_serial_probe,
+	.disconnect =	usb_serial_console_disconnect,
 };
 
 static struct usb_serial_device_type siemens_usb_mpi_device = {
@@ -48,6 +51,12 @@ static struct usb_serial_device_type siemens_usb_mpi_device = {
 	.num_ports =		1,
 };
 
+static void __exit siemens_usb_mpi_exit(void)
+{
+	usb_deregister(&siemens_usb_mpi_driver);
+	usb_serial_deregister_drivers(&siemens_usb_mpi_device);
+}
+
 static int __init siemens_usb_mpi_init(void)
 {
 	int retval;
@@ -57,7 +66,7 @@ static int __init siemens_usb_mpi_init(void)
 		goto failed_usb_serial_register;
 	retval = usb_register(&siemens_usb_mpi_driver);
 	if (retval)
-		goto failed_usb_register;	
+		goto failed_usb_register;
 	info (DRIVER_DESC);
 	info (DRIVER_VERSION " " DRIVER_AUTHOR);
 	return retval;
@@ -67,11 +76,6 @@ failed_usb_serial_register:
 	return retval;
 }
 
-static void __exit siemens_usb_mpi_exit(void)
-{
-	usb_deregister(&siemens_usb_mpi_driver);
-	usb_serial_deregister(&siemens_usb_mpi_device);
-}
 
 module_init(siemens_usb_mpi_init);
 module_exit(siemens_usb_mpi_exit);
