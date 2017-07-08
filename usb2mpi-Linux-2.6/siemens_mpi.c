1e132673d38b539d9480a7f9694fe0a5073c9961
victor996 <victor.dcavalcanti@yahoo.com.br>
2017-07-08 13:54:01 -0300

    juse

--- usb2mpi-Linux-2.6/siemens_mpi.c
+++ usb2mpi-Linux-2.6/siemens_mpi.c
@@ -1,15 +1,15 @@
 /*
- * Siemens USB-MPI Serial USB driver
- *
- * Copyright (C) 2005 Thomas Hergenhahn <thomas.hergenhahn@suse.de>
- *
- * Largely based on siemens_usb_mpi.c,
- * Copyright (C) 2005 Greg Kroah-Hartman <gregkh@suse.de>
- *
- *	This program is free software; you can redistribute it and/or
- *	modify it under the terms of the GNU General Public License version
- *	2 as published by the Free Software Foundation.
- */
+* Siemens USB-MPI Serial USB driver
+*
+* Copyright (C) 2005 Thomas Hergenhahn <thomas.hergenhahn@suse.de>
+*
+* Largely based on siemens_usb_mpi.c,
+* Copyright (C) 2005 Greg Kroah-Hartman <gregkh@suse.de>
+*
+*	This program is free software; you can redistribute it and/or
+*	modify it under the terms of the GNU General Public License version
+*	2 as published by the Free Software Foundation.
+*/
 
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -34,55 +34,61 @@ MODULE_DEVICE_TABLE(usb, id_table);
 
 static int siemens_usb_mpi_probe(struct usb_interface *interface,
 	const struct usb_device_id *id){
-return 0;
-};
+		return 0;
+	};
 
-static struct usb_driver siemens_usb_mpi_driver = {
-	// .owner =	THIS_MODULE,
-	.name = NAME,
-	.id_table =	id_table,
-        .probe =	siemens_usb_mpi_probe,
-	.disconnect =	siemens_usb_mpi_exit,
-};
+	static void __exit siemens_usb_mpi_exit(void)
+	{
+		usb_deregister(&siemens_usb_mpi_driver);
+		usb_serial_deregister_drivers(&siemens_usb_mpi_device);
+	}
 
-static struct usb_serial_driver siemens_usb_mpi_device = {
-	.owner =		THIS_MODULE,
-	.name =			"Siemens USB-MPI",
-	.id_table =		id_table,
-	.num_interrupt_in =	NUM_DONT_CARE,
-	.num_bulk_in =		NUM_DONT_CARE,
-	.num_bulk_out =		NUM_DONT_CARE,
-	.num_ports =		1,
-};
+	static struct usb_driver siemens_usb_mpi_driver = {
+		// .owner =	THIS_MODULE,
+		.name = NAME,
+		.id_table =	id_table,
+		.probe =	siemens_usb_mpi_probe,
+		.disconnect =	siemens_usb_mpi_exit,
+	};
 
-static void __exit siemens_usb_mpi_exit(void)
-{
-	usb_deregister(&siemens_usb_mpi_driver);
-	usb_serial_deregister_drivers(&siemens_usb_mpi_device);
-}
+	static struct usb_serial_driver siemens_usb_mpi_device = {
+		.owner =		THIS_MODULE,
+		.name =			"Siemens USB-MPI",
+		.id_table =		id_table,
+		.num_interrupt_in =	NUM_DONT_CARE,
+		.num_bulk_in =		NUM_DONT_CARE,
+		.num_bulk_out =		NUM_DONT_CARE,
+		.num_ports =		1,
+	};
 
-static int __init siemens_usb_mpi_init(void)
-{
-	int retval;
+	static void __exit siemens_usb_mpi_exit(void)
+	{
+		usb_deregister(&siemens_usb_mpi_driver);
+		usb_serial_deregister_drivers(&siemens_usb_mpi_device);
+	}
 
-	retval = usb_serial_register(&siemens_usb_mpi_device);
-	if (retval)
+	static int __init siemens_usb_mpi_init(void)
+	{
+		int retval;
+
+		retval = usb_serial_register(&siemens_usb_mpi_device);
+		if (retval)
 		goto failed_usb_serial_register;
-	retval = usb_register(&siemens_usb_mpi_driver);
-	if (retval)
+		retval = usb_register(&siemens_usb_mpi_driver);
+		if (retval)
 		goto failed_usb_register;
-	info (DRIVER_DESC);
-	info (DRIVER_VERSION " " DRIVER_AUTHOR);
-	return retval;
-failed_usb_register:
+		info (DRIVER_DESC);
+		info (DRIVER_VERSION " " DRIVER_AUTHOR);
+		return retval;
+		failed_usb_register:
 		usb_serial_deregister(&siemens_usb_mpi_device);
-failed_usb_serial_register:
-	return retval;
-}
+		failed_usb_serial_register:
+		return retval;
+	}
 
 
-module_init(siemens_usb_mpi_init);
-module_exit(siemens_usb_mpi_exit);
-MODULE_AUTHOR( DRIVER_AUTHOR );
-MODULE_DESCRIPTION( DRIVER_DESC );
-MODULE_LICENSE( "GPL" );
+	module_init(siemens_usb_mpi_init);
+	module_exit(siemens_usb_mpi_exit);
+	MODULE_AUTHOR( DRIVER_AUTHOR );
+	MODULE_DESCRIPTION( DRIVER_DESC );
+	MODULE_LICENSE( "GPL" );
